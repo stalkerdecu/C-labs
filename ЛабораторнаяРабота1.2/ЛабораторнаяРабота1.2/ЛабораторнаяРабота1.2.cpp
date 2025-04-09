aa10fc54446c7019c1cdaca5a5f1dfbe09e6d4c4
stalkerdecu <92627701+stalkerdecu@users.noreply.github.com>
2025-04-09 23:59:26 +0300

    Add files via upload

---
+++ ЛабораторнаяРабота1.2/ЛабораторнаяРабота1.2/ЛабораторнаяРабота1.2.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <string>
+
+class Entity {
+protected:
+    std::string name; // Защищенное поле: имя
+    int health;      // Защищенное поле: здоровье
+
+public:
+    // Конструктор базового класса
+    Entity(const std::string& n, int h) : name(n), health(h) {}
+
+    // Метод для вывода информации
+    virtual void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health <<  std::endl;
+    }
+
+    virtual ~Entity() {}
+};
+
+class Player : public Entity {
+private:
+    int experience; // Приватное поле: опыт
+
+public:
+    // Конструктор производного класса
+    Player(const std::string& n, int h, int exp)
+        : Entity(n, h), experience(exp) {}
+
+    // Переопределение метода displayInfo
+    void displayInfo() const override {
+        Entity::displayInfo(); // Вызов метода базового класса
+        std::cout << "Experience: " << experience << std::endl;
+    }
+};
+
+class Enemy : public Entity {
+private:
+    std::string type; // Приватное поле: тип врага
+
+public:
+    // Конструктор производного класса
+    Enemy(const std::string& n, int h, const std::string& t)
+        : Entity(n, h), type(t) {}
+
+    // Переопределение метода displayInfo
+    void displayInfo() const override {
+        Entity::displayInfo(); // Вызов метода базового класса
+        std::cout << "Type: " << type << std::endl;
+    }
+};
+
+
+
+
+
+
+
+class Boss : public Enemy {
+    std::string specialAbility = "meat hook";
+public:
+    Boss(const std::string& n, int h, const std::string& t)
+        : Enemy(n, h, t){}
+    void displayInfo() const override {
+        Entity::displayInfo(); 
+        std::cout << "special ability: " << specialAbility << std::endl;
+    }
+};
+
+
+
+
+
+
+int main() {
+    // Создаем объекты игрока и врага
+    Player hero("Hero", 100, 0);
+    Enemy monster("Goblin", 50, "Goblin");
+    Boss boss("nightmare", 200, "Boss");
+
+    // Выводим информацию о персонажах
+    hero.displayInfo();
+    monster.displayInfo();
+    boss.displayInfo();
+
+    return 0;
+}
