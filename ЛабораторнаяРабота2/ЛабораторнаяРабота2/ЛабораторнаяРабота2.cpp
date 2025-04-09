0177a172261ac94eae1e0dbcaa50ec28a8836054
stalkerdecu <92627701+stalkerdecu@users.noreply.github.com>
2025-04-10 00:00:02 +0300

    Add files via upload

---
+++ ЛабораторнаяРабота2/ЛабораторнаяРабота2/ЛабораторнаяРабота2.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Character " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Character() {
+        std::cout << "Character " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Monster {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Monster(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Monster " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Monster() {
+        std::cout << "Monster " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+
+
+
+
+
+class Weapon {
+private:
+    std::string name;
+    int damage;
+    int weight;
+
+public:
+    // Конструктор
+    Weapon(const std::string& n, int d, int w)
+        : name(n), damage(d), weight(w) {
+        std::cout << "Weapon " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Weapon() {
+        std::cout << "Weapon " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Naming: " << name << ", DPS: " << damage
+            << ", Weight: " << weight << std::endl;
+    }
+};
+int main() {
+    // Создаем объекты персонажей
+    Weapon Weapon1("sword", 10, 5);
+    Weapon Weapon2("pistol", 45, 1);
+
+    Weapon1.displayInfo();
+    Weapon2.displayInfo();
+
+
+
+    return 0;
+}
